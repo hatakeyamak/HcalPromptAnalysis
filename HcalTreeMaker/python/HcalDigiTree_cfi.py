d5aa3488079936af9b0196d34e61f7bff2be8357
Christopher Madrid <Chris_Madrid@baylor.edu>
2017-05-21 18:26:07 -0500

    Missed some changed from last commit

--- HcalTreeMaker/python/HcalDigiTree_cfi.py
+++ HcalTreeMaker/python/HcalDigiTree_cfi.py
@@ -1,9 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-hcalRecHitTree = cms.EDAnalyzer("RecHitTree",
-    rootOutputFile            = cms.string('HcalRecHitSpectra.root'),
+hcalDigiTree = cms.EDAnalyzer("RecHitTree",
+    rootOutputFile            = cms.string('HcalDigi.root'),
     treeName                  = cms.string('HcalRecHit'),
     TestNumbering             = cms.bool(True),
     HBHERecHitCollectionLabel = cms.untracked.InputTag("hbheUpgradeReco"),
     HFRecHitCollectionLabel   = cms.untracked.InputTag("hfUpgradeReco")
 )
+
+#hcalRecHitTree = cms.EDAnalyzer("RecHitTree",
+#    rootOutputFile            = cms.string('HcalRecHitSpectra.root'),
+#    treeName                  = cms.string('HcalRecHit'),
+#    TestNumbering             = cms.bool(True),
+#    HBHERecHitCollectionLabel = cms.untracked.InputTag("hbheUpgradeReco"),
+#    HFRecHitCollectionLabel   = cms.untracked.InputTag("hfUpgradeReco")
+#)
