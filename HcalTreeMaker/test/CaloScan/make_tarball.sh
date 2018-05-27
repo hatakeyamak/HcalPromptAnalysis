df1da6a4cdb80bfc4b38b62df85a12c199db7d38
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2018-05-26 21:57:41 -0500

    cleanup

--- HcalTreeMaker/test/CaloScan/make_tarball.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export MYCMSSW=`echo $CMSSW_BASE | rev | cut -d'/' -f-1 | rev`
-
-cd ${CMSSW_BASE}/..
-tar -zcvf /cms/data/store/user/${USER}/condor/${MYCMSSW}_condor.tgz ${MYCMSSW} --exclude=tmp --exclude=test --exclude='*.root'
-cd -
-
